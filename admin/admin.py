e72f81fc6e2331dd982b281745827e77aa6b83f4
gsam1 <gsamardjiev@gmail.com>
2019-01-09 23:56:03 +0200

    Made slight modifications to the object retrieval. More work to follow

--- admin/admin.py
+++ admin/admin.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import jsonify
 from clients import ClientDocker
 from config import admin_config
 
 ENV = 'development'
 admin = Flask(__name__)
+config = admin_config[ENV]()
+cld = ClientDocker(config.docker_url)
 
 @admin.route('/')
 def index():
@@ -11,10 +14,13 @@ def index():
 
 @admin.route('/get_all_containers')
 def get_all_containers():
-    config = admin_config[ENV]()
-    cld = ClientDocker(config.docker_url)
-    containers_list = str(cld.get_all_containers())
-    return containers_list
+    # containers_list = str(cld.get_all_containers())
+    return jsonify(cld.get_all_containers())
+
+@admin.route('/get_active_containers')
+def get_active_containers():
+    # containers_list = str(cld.get_active_containers())
+    return jsonify(cld.get_active_containers())
 
 
 if __name__ == '__main__':
--- admin/clients/client_docker.py
+++ admin/clients/client_docker.py
@@ -13,23 +13,30 @@ ENV = 'development'
 class ClientDocker(object):
     def __init__(self, url):
         self.client = docker.DockerClient(base_url=url)
+
+    def _create_container_obj_list(self, cntrlist):
+        container_list = []
+        for container in cntrlist:
+            container_list.append({
+                'name': container.name,
+                'spec': container.attrs
+            })
+
+        return container_list
     
     def get_active_containers(self):
-        return self.client.containers.list()
+        active_containers = self.client.containers.list()
+        return self._create_container_obj_list(active_containers)
 
     def get_all_containers(self):
-        container_list = self.client.containers.list(all=True)
-        all_containers = []
-        for container in container_list:
-            all_containers.append(container.name)
-        
-        return all_containers
+        all_containers = self.client.containers.list(all=True)
+        return self._create_container_obj_list(all_containers)
 
 
 def main():
     config = admin_config[ENV]()
     cld = ClientDocker(config.docker_url)
-    print(cld.get_all_containers())
+    print(cld.get_active_containers())
 
 
 if __name__ == '__main__':
