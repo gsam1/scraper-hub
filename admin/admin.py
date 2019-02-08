bc019b5bd6d174a7c4e338a23f5099cecf29b38a
gsam1 <gsamardjiev@gmail.com>
2019-02-08 17:20:41 +0200

    Admin API cleanup

--- admin/admin.py
+++ admin/admin.py
@@ -39,6 +39,7 @@ def run_container():
     volume = image_map[req_json['type']][req_json['distribution']]['data_location']
     env_vars = image_map[req_json['type']][req_json['distribution']]['env_vars']
     cntr_id = cld.run_container(image, port, volume, env_vars)
+    
     return f'{cntr_id}'
 
 if __name__ == '__main__':
